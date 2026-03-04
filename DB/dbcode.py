c9e2edb2245b62032acd4627e95e80e55b8ea445
Dex <jeppeandersen@outlook.dk>
2026-03-04 20:33:34 +0100

    someting with sql

--- DB/dbcode.py
+++ DB/dbcode.py
@@ -1,2 +1,10 @@
 import sqlite3
 
+def add_to_db():
+ a = 1
+#with sqlite3.connect( <name of bd in her> ) as conn:
+
+#cursor = conn.cursor()
+
+#cursor.execute(INSERT INTO table_name(c1, c2 ) VALUES (?, ?))
+
