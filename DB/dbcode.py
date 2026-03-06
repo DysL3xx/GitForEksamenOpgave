7e720e49da2ff4bb96e925e5f41111aac9abfeef
Dex <jeppeandersen@outlook.dk>
2026-03-06 10:16:37 +0100

    db code

--- DB/dbcode.py
+++ DB/dbcode.py
@@ -1,10 +1,18 @@
 import sqlite3
 
-def add_to_db():
- a = 1
+database = "databaseforeksamenstuffplzgodholpimtrapedinthishellhole.db"
+
+def add_character_and_initiative(character_name, initiative):
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO characters (name, initiative) values (?, ?)", (character_name, initiative))
+        conn.commit()
+
 #with sqlite3.connect( <name of bd in her> ) as conn:
 
 #cursor = conn.cursor()
 
 #cursor.execute(INSERT INTO table_name(c1, c2 ) VALUES (?, ?))
+
+
 
