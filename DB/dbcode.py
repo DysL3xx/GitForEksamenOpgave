bae5ed730aa4d39504f00c90cfc6f3d2e9e4fcc9
Dex <jeppeandersen@outlook.dk>
2026-03-12 11:03:42 +0100

    sometig that sould work

--- DB/dbcode.py
+++ DB/dbcode.py
@@ -1,18 +1,22 @@
 import sqlite3
 
-database = "databaseforeksamenstuffplzgodholpimtrapedinthishellhole.db"
+DB = "databaseforeksamenstuffplzgodholpimtrapedinthishellhole.db"
 
 def add_character_and_initiative(character_name, initiative):
-    with sqlite3.connect(database) as conn:
+    with sqlite3.connect(DB) as conn:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO characters (name, initiative) values (?, ?)", (character_name, initiative))
         conn.commit()
+    
+def delete_character(character_name):
+    with sqlite3.connect(DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM characters WHERE name = ?", (character_name,))
+        conn.commit()
+
 
 #with sqlite3.connect( <name of bd in her> ) as conn:
 
 #cursor = conn.cursor()
 
 #cursor.execute(INSERT INTO table_name(c1, c2 ) VALUES (?, ?))
-
-
-
